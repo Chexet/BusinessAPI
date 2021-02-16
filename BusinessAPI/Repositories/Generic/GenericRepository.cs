67b90f52e87be4f0857be8eccc7e55c06cfce582
Gustav Pettersson <GustavPettersson@live.se>
2021-02-16 23:16:09 +0100

    Liiiittle init of generic repo

--- BusinessAPI/Repositories/Generic/GenericRepository.cs
+++ BusinessAPI/Repositories/Generic/GenericRepository.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using BusinessAPI.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BusinessAPI.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<TEntity, TQuery> where TEntity : class
     {
+        protected readonly BusinessContext businessContext;
+        protected IQueryable<TEntity> dbSet;
+
+        public GenericRepository(BusinessContext context)
+        {
+            businessContext = context;
+            dbSet = businessContext.Set<TEntity>().AsQueryable();
+        }
     }
 }
