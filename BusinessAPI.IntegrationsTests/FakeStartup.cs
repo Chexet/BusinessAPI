6950b73d6d486db85adfed0b9241dcfb47047483
Gustav Pettersson <GustavPettersson@live.se>
2021-03-01 11:52:08 +0100

    Begun setting up testing env

---
+++ BusinessAPI.IntegrationsTests/FakeStartup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessAPI.IntegrationTests
+{
+    public static class FakeStartup
+    {
+        public static IServiceCollection SetupTestDatabase<TContext>(this IServiceCollection services, string name) where TContext : DbContext
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<TContext>));
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddScoped(p =>
+                DbContextOptionsFactory<TContext>(
+                    p,
+                    (sp, options) => options
+                        .ConfigureWarnings(b => b.Log(CoreEventId.ManyServiceProvidersCreatedWarning))
+                        .UseInMemoryDatabase(name)));
+
+            return services;
+        }
+
+        private static DbContextOptions<TContext> DbContextOptionsFactory<TContext>(
+            IServiceProvider applicationServiceProvider,
+            Action<IServiceProvider, DbContextOptionsBuilder> optionsAction)
+            where TContext : DbContext
+        {
+            var builder = new DbContextOptionsBuilder<TContext>(
+                new DbContextOptions<TContext>(new Dictionary<Type, IDbContextOptionsExtension>()));
+
+            builder.UseApplicationServiceProvider(applicationServiceProvider);
+
+            optionsAction?.Invoke(applicationServiceProvider, builder);
+
+            return builder.Options;
+        }
+    }
+}
---
+++ BusinessAPI.IntegrationsTests/IntegrationTest.cs
@@ -0,0 +1,39 @@
+﻿using BusinessAPI.Contexts;
+using BusinessAPI.Controllers;
+using BusinessAPI.IntegrationTests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessAPI.IntegrationsTests
+{
+    class IntegrationTest
+    {
+        protected readonly IServiceProvider serviceProvider;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.SetupTestDatabase<BusinessContext>(Guid.NewGuid().ToString());
+                        services.AddScoped<OrganizationController>();
+                        services.AddScoped<TeamController>();
+                        services.AddScoped<UserController>();
+                        services.AddScoped<RoleController>();
+                    });
+                });
+        }
+
+        public void Dispose()
+        {
+            using var serviceScope = serviceProvider.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<BusinessContext>();
+            context.Database.EnsureDeleted();
+        }
+    }
+}
